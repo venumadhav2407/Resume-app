094c8b0148752b97848b507f194c306c3c194631
Venu Madhav <113613653+venumadhav2407@users.noreply.github.com>
2024-10-12 14:18:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ with st.sidebar:
 
 # About Section
 if selected == "About":
-    st.info("Experienced Software Developer transitioning into Data Science. Skilled in Python, SQL, PowerBI, and Excel for data visualization. Proficient in web app development, database management, and data manipulation. Leveraging analytical skills for Data Science role.")
+    col1, col2 = st.columns(2)
+    col1.info("Experienced Software Developer transitioning into Data Science. Skilled in Python, SQL, PowerBI, and Excel for data visualization. Proficient in web app development, database management, and data manipulation. Leveraging analytical skills for Data Science role.")
+    col2.image("img.jpg")
 # Skills Section
 elif selected == "Skills":
     st.title("Skills")
